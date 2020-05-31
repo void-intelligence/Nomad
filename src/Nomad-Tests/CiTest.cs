72f7fa15e3fa137def44a33078308f213cc03e04
Nirex <nirex.0@gmail.com>
2020-05-31 22:25:22 +0430

    Create CiTest.cs

---
+++ src/Nomad-Tests/CiTest.cs
@@ -0,0 +1,20 @@
+// © 2020 VOID-INTELLIGENCE ALL RIGHTS RESERVED
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nomad.Core;
+
+namespace NomadTest
+{
+    [TestClass]
+    public class CiTest
+    {
+        [TestMethod]
+        public void CiTestMethod()
+        {
+
+            Assert.IsTrue(true, "CI: Initial Test Successful");
+        }
+    }
+
+}
