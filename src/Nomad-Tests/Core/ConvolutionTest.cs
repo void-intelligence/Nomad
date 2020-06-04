759ab125836471ebcc8543e2dfb73bab61171321
Nirex <nirex.0@gmail.com>
2020-06-05 00:50:36 +0430

    Add: Convolution Test

---
+++ src/Nomad-Tests/Core/ConvolutionTest.cs
@@ -0,0 +1,28 @@
+﻿// © 2020 VOID-INTELLIGENCE ALL RIGHTS RESERVED
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nomad.Core;
+
+namespace NomadTest.Core
+{
+    [TestClass]
+    public class ConvolutionTest
+    {
+
+        [TestMethod]
+        public void Convolution()
+        {
+            // ReSharper disable NotAccessedVariable
+            // ReSharper disable RedundantAssignment
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+            var mat = new Matrix(10, 10);
+            mat.InRandomize();
+            var x = mat.Duplicate();
+            mat = mat.Convolve(Matrix.Filters.Sobel(), 1, 1);
+            mat = mat.Convolve(Matrix.Filters.Sobel(), 1, 1, 0, 4);
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+            // ReSharper enable RedundantAssignment
+            // ReSharper enable NotAccessedVariable
+        }
+    }
+}
