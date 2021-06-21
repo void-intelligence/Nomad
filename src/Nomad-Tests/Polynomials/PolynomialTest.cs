cb089f54dd7cd6a16ce4466f735d5d5b7ae6a7f3
Nirex <Nirex.0@gmail.com>
2021-06-21 09:54:38 +0430

    Begin Polynomial Implementation

---
+++ src/Nomad-Tests/Polynomials/PolynomialTest.cs
@@ -0,0 +1,22 @@
+﻿// © 2021 VOID-INTELLIGENCE ALL RIGHTS RESERVED
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nomad.Polynomials;
+
+namespace NomadTest.Polynomials
+{
+    [TestClass]
+    public class PolynomialTest
+    {
+        [TestMethod]
+        public void PolynomialEvaluation()
+        {
+            // 1x^0 + 2x^1 + 3x^2
+            double[] coeffs = { 4, 2, 3 };
+
+            Polynomial p1 = new Polynomial(coeffs);
+            Assert.AreEqual(p1.Evaluate(2), 20);
+        }
+
+    }
+}
---
+++ src/Nomad/Polynomials/Polynomial.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nomad.Polynomials
+{
+    public class Polynomial
+    {
+        public List<double> Coefficients;
+        public double Value;
+
+        public Polynomial()
+        {
+            Value = 0;
+            Coefficients = new List<double> { 1 };
+        }
+
+        public Polynomial(double value)
+        {
+            Value = 0;
+            Coefficients = new List<double> { value };
+        }
+
+        public Polynomial(List<double> Coeffs)
+        {
+            Value = 0;
+            Coefficients = Coeffs;
+        }
+
+        public Polynomial(Polynomial copy)
+        {
+            Value = 0;
+            Coefficients = copy.Coefficients;
+        }
+
+        public Polynomial(double[] Coeffs)
+        {
+            Value = 0;
+            Coefficients = Coeffs.ToList();
+        }
+
+        public double Evaluate(double X)
+        {
+            double result = 0;
+            for (int i = Coefficients.Count - 1; i >= 0 ; i--)
+                result += Math.Pow(X, i) * Coefficients[i];
+            return result;
+        }
+
+        public static Polynomial operator+(Polynomial first, Polynomial second)
+        {
+
+            return new Polynomial();
+        }
+    }
+}
